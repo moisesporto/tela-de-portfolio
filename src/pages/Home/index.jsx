943cc5cf613cc1efbabbf957a66562e8f506f967
moisesporto <moisesporto@hotmail.com>
2025-03-26 22:20:40 -0300

    container quase pronto

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,26 +1,30 @@
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import { Link } from 'react-router-dom'
+import Container from '../../components/Container'
+import styles from './Home.module.css'
 
 function Home() {
     return (
         <>
             <Header />
-            <section className='container'>
-                <div className='apresentacao'>
+            <Container>
+                <section className={styles.home}>
+                <div className={styles.apresentacao}>
                     <p>
                         Olá, sou <br />
                         <span> Moisés Porto</span> <br />
                         Dev Full Stack
                     </p>
-                    <Link to="/sobre" className='btn btn-red'>
+                    <Link to="/sobre" className={`${styles.btn} ${styles.btn_red}`}>
                         Saiba mais sobre mim
                     </Link>
                 </div>
                 <figure>
-                    <img className='img-home' src="/developer-red.svg" alt="Imagem de Home" />
+                    <img className={styles.img_home} src="/developer-red.svg" alt="Imagem de Home" />
                 </figure>
-            </section>
+                </section>
+            </Container>
             <Footer />
         </>
     )
